4ac0e5d7d71899852aba0c86795590116221ca4c
Anyikabz <anyikabsdev@gmail.com>
2025-06-07 15:01:04 +0100

    Admin section updated

---
+++ Question2.cs
@@ -0,0 +1,8 @@
+public class Question
+{
+    public string QuestionText { get; set; }
+    public string CorrectAnswer { get; set; }
+    public int Points { get; set; }
+    public int BonusPoints { get; set; }
+    public string ImagePath { get; set; }
+}
--- QuizCompetitionApp/Question.cs
+++ QuizCompetitionApp/Question.cs
@@ -1,15 +1,23 @@
-﻿// File: Question.cs
-using System.Collections.Generic;
-
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
 namespace QuizCompetitionApp
 {
     public class Question
     {
-        public string QuestionText { get; set; }
-        public List<string> AnswerOptions { get; set; }
-        public List<int> CorrectAnswerIndices { get; set; } = new List<int>();
-        public int Points { get; set; }
-        public int BonusPoints { get; set; }
-        public string ImagePath { get; set; }
+        public string Text { get; set; } = string.Empty;
+        public List<string> Options { get; set; } = new List<string>();
+        public List<int> CorrectAnswers { get; set; } = new List<int>();
+        public string ImagePath { get; set; } = string.Empty;
+        public int Point { get; set; }
+        public int BonusPoint { get; set; }
+
+        [JsonIgnore]
+        public string DisplayText { get; set; } = string.Empty;
+
+        public override string ToString()
+        {
+            return DisplayText;
+        }
     }
 }
+
